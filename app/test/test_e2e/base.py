aca202464e7d3a194ad8fc035adda261ea0e8499
Joaquín Luberto <joaquinluberto@gmail.com>
2025-06-08 23:31:28 -0300

    Se corrige base.py tras revisión con profesores

--- app/test/test_e2e/base.py
+++ app/test/test_e2e/base.py
@@ -1,38 +1,49 @@
 import os
 
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import Browser, sync_playwright
+
 
 from app.models import User
 
+
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+playwright = sync_playwright().start()
 headless = os.environ.get("HEADLESS", 1) == 1
 slow_mo = os.environ.get("SLOW_MO", 0)
+
+
 
 
 class BaseE2ETest(StaticLiveServerTestCase):
     """Clase base con la configuración común para todos los tests E2E"""
 
+
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.playwright = sync_playwright().start()
-        cls.browser = cls.playwright.chromium.launch(headless=headless, slow_mo=int(slow_mo))
+        cls.browser: Browser = playwright.chromium.launch(
+            headless=headless, slow_mo=int(slow_mo)
+        )
+
 
     @classmethod
     def tearDownClass(cls):
-        cls.browser.close()
-        cls.playwright.stop()
         super().tearDownClass()
+        cls.browser.close()
+
 
     def setUp(self):
         # Crear un contexto y página de Playwright
         self.context = self.browser.new_context()
         self.page = self.context.new_page()
 
+
     def tearDown(self):
         # Cerrar la página después de cada test
         self.page.close()
+
 
     def create_test_user(self, is_organizer=False):
         """Crea un usuario de prueba en la base de datos"""
@@ -42,6 +53,7 @@ class BaseE2ETest(StaticLiveServerTestCase):
             password="password123",
             is_organizer=is_organizer,
         )
+
 
     def login_user(self, username, password):
         """Método auxiliar para iniciar sesión"""
